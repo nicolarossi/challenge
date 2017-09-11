ca04bc37636e0d8402da3b34222f13ef6f5a8e28
Nicola <nicola.rossi+2@gmail.com>
2017-09-11 14:10:04 +0200

    Cleaning

--- src/challenge.cpp
+++ src/challenge.cpp
@@ -1,9 +1,7 @@
 //============================================================================
 // Name        : challenge.cpp
 // Author      : Nicola Rossi
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Unified Streaming challenge. Extract audio/video from a TS stream
 //============================================================================
 
 #include <iostream>
@@ -11,21 +9,21 @@
 
 using namespace std;
 
-void print_usage(char *exe){
+int print_usage(char *exe){
     cerr<<" Error in usage:"<<endl;
     cerr<<"\t"<<exe<<" <input_file>" << endl;
-    exit(-1);
+    return (-1);
 }
 
 int main(int argc,char**argv) {
 
     if (argc!=2) {
-        print_usage(argv[0]);
+        return print_usage(argv[0]);
     }
+
     ifstream ifs(argv[1],std::ifstream::binary);
 
     challenge::demuxer D;
-
     ifs >> D;
 
 	return 0;
