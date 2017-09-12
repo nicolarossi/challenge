8dce9a405678094681dd92a9fff0291cb3c34a32
Nicola <nicola.rossi+2@gmail.com>
2017-09-12 14:11:13 +0200

    Updated IDE automatically generated makefile

--- Debug/src/subdir.mk
+++ Debug/src/subdir.mk
@@ -4,44 +4,32 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
-../src/PACKET.cpp \
-../src/PACKET_HEADER.cpp \
-../src/PAS.cpp \
-../src/PAT.cpp \
+../src/Demuxer.cpp \
 ../src/PES_packet.cpp \
-../src/SYSTEM_HEADER.cpp \
+../src/TS_Packet.cpp \
 ../src/challenge.cpp \
-../src/common_tool.cpp \
-../src/demuxer.cpp 
+../src/common_tool.cpp 
 
 OBJS += \
-./src/PACKET.o \
-./src/PACKET_HEADER.o \
-./src/PAS.o \
-./src/PAT.o \
+./src/Demuxer.o \
 ./src/PES_packet.o \
-./src/SYSTEM_HEADER.o \
+./src/TS_Packet.o \
 ./src/challenge.o \
-./src/common_tool.o \
-./src/demuxer.o 
+./src/common_tool.o 
 
 CPP_DEPS += \
-./src/PACKET.d \
-./src/PACKET_HEADER.d \
-./src/PAS.d \
-./src/PAT.d \
+./src/Demuxer.d \
 ./src/PES_packet.d \
-./src/SYSTEM_HEADER.d \
+./src/TS_Packet.d \
 ./src/challenge.d \
-./src/common_tool.d \
-./src/demuxer.d 
+./src/common_tool.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -O0 -g -pg -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
